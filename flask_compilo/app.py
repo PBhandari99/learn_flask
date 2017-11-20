8168d4fd566adb189b72a945125d2ca549d4494e
PBhandari99 <prashantb1097@gmail.com>
2017-11-20 01:27:29 -0500

    added method for interecting with db

--- flask_compilo/app.py
+++ flask_compilo/app.py
@@ -1,9 +1,8 @@
 import os
 #  import tarfile
-from flask import Flask, request, redirect, jsonify
+import sqlite3
+from flask import Flask, request, redirect, jsonify, make_response
 from werkzeug.utils import secure_filename
-
-from  compiler.compiler import *
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['pdf', 'tar', '.cpp', 'cc', '.h', '.hpp'])
@@ -42,9 +41,70 @@ def submit_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return jsonify(status="uploaded")
+        return make_response(jsonify(status="uploaded"))
 
 #  def unzip_tar_file(file_to_unzip):
     #  tar = tarfile.open(file_to_unzip)
     #  tar.extractall('./uploads/extracts')
     #  tar.close()
+
+
+def init_db():
+    # Add timeOut to close the db in case of slow connection, default is 5s.
+    db_connect = sqlite3.connect("database.db")
+    return db_connect
+
+
+def create_table(table):
+    db = init_db()
+    db_cursor = db.cursor()
+    # TODO: Fix this shit, it's too long.
+    # I guess you can also do "?" for the tabel name, make sure you get rid of {0}.
+    query = "CREATE TABLE {0} (USER_ID  TEXT NOT NULL, assignment TEXT NOT NULL,".format(table) +
+            "file BLOB NOT NULL, compile_error text, run_time_error text, result text)"
+    db_cursor.execute(query)
+    db.commit()
+    db.close()
+
+def write_file_result_to_db(user_id, assignment, file, table, compile_err=None,
+                            run_time_err=None, result=None):
+    db = init_db()
+    db_cursor = db.cursor()
+    if compile_err:
+        data = (user_id, assignment, file, compile_err)
+    elif run_time_err:
+        data = (user_id, assignment, file, run_time_err)
+    elif result:
+        data = (user_id, assignment, file, result)
+    else:
+        return -1
+    db_cursor.execute("INSERT INTO {0} VALUES(?, ?, ?, ?, ?, ?)".format(table),
+                      data)
+    db.commit()
+    db.close()
+
+#  implement this method if you decide to continue working on it later.
+#  def clear_table():
+
+def get_results_by_users(table, user_id):
+    db = init_db()
+    db_cursor = db.cursor()
+    if user_id:
+        data = (user_id,)
+        row = db_cursor.execute("SELECT * FROM {0} WHERE USER_ID=?".format(table), data).fetchone()
+        db.close()
+        return row
+    db.close()
+    return "No user_id"
+
+def get_all_results(table):
+    db = init_db()
+    db_cursor = db.cursor()
+    row = db_cursor.execute("SELECT * FROM {0}".format(table)).fetchone()
+    db.close()
+    return row
+
+
+
+
+
